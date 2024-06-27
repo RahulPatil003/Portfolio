d69f6ea2e550f56670ffa62d12333b3bfd0c0d52
RAHULPATIL22003 <rahulpatilpr483@gmail.com>
2024-06-27 23:42:14 +0530

    added about and skills page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import "./App.css";
 import Header from './Components/Header/Header'
 import Home from './Components/Home/Home'
+import About from './Components/About/About';
+import Skills from './Components/Skills/Skills';
 const App = () => {
   return (
     <div>
       <Header></Header>
       <main className='main'>
-        <Home/>
+        <Home />
+        <About />
+        <Skills />
       </main>
     </div>
   )
